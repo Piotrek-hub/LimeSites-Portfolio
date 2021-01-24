bd35ea7469318be57ae3bf888d565d9d0ad46f32
c0ndi <73940688+c0ndi@users.noreply.github.com>
2021-01-24 14:49:47 +0100

    folder js
    do dokonczenia duzo rzeczy jutro zaczynam dark mode

---
+++ scripts/animation.js
@@ -0,0 +1,6 @@
+const contactPhotoImg = [...document.querySelectorAll('.contact-logos')];
+
+
+/* 
+contactPhotoImg[0].addEventListener('mouseover', showPhoto)
+contactPhotoImg[0].addEventListener('mouseover', hidePhoto) */
