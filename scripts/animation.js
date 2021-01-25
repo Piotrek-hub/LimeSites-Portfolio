b914f8d5de0d120af820eea986508864daab2404
c0ndi <73940688+c0ndi@users.noreply.github.com>
2021-01-25 10:22:05 +0100

    Ogolne zmiany poprawa jakości kodu

--- scripts/animation.js
+++ scripts/animation.js
@@ -4,3 +4,5 @@ const contactPhotoImg = [...document.querySelectorAll('.contact-logos')];
 /* 
 contactPhotoImg[0].addEventListener('mouseover', showPhoto)
 contactPhotoImg[0].addEventListener('mouseover', hidePhoto) */
+
+
