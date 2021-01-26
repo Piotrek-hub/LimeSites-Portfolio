b911f8c278540ae7b1f0c141a009ed03166b2faf
c0ndi <73940688+c0ndi@users.noreply.github.com>
2021-01-26 20:55:13 +0100

    lepszy kod js

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,17 +1,23 @@
 let activeDarkMode = false;
-
+const darkModeBtn = document.querySelector('.dark-mode-test');
+const navbar = document.querySelector('.navbar');
+const arrows = [...document.querySelectorAll('.arrow')];
 document.querySelector('.dark-mode-test').addEventListener('click', () => { 
     activeDarkMode = !activeDarkMode;
     if(activeDarkMode)
     {
         document.body.style.setProperty('--white', '#212121');
         document.body.style.setProperty('--black', '#fafafa');
-        document.querySelector('.dark-mode-test').textContent = 'LightMode';
-        document.querySelector('.navbar').style.boxShadow = 'none';
+        darkModeBtn.textContent = 'LightMode';
+        navbar.style.boxShadow = 'none';
+        arrows.forEach(arrow => {
+            arrow.src = 'assets/down-arrow-white.svg';
+        })
     }else{ 
         document.body.style.setProperty('--white', '#fafafa');
         document.body.style.setProperty('--black', '#212121');
-        document.querySelector('.dark-mode-test').textContent = 'DarkMode';
-        document.querySelector('.navbar').style.boxShadow = '0px 1px 100px 0px rgba(0,0,0,0.1)';
+        darkModeBtn.textContent = 'DarkMode';
+        navbar.style.boxShadow = '0px 1px 100px 0px rgba(0,0,0,0.1)';
+        arrow.src = 'assets/arrow.svg';
     }
 });
