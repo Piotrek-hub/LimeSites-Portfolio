2c3c72f261da481671cef91040bd7e6beb2b4081
Piotrek <piotrgszkola@gmail.com>
2021-01-26 09:42:34 +0100

    update

--- scripts/animation.js
+++ scripts/animation.js
@@ -12,5 +12,6 @@ document.querySelector('.dark-mode-test').addEventListener('click', () => {
         document.body.style.setProperty('--white', '#fafafa');
         document.body.style.setProperty('--black', '#212121');
         document.querySelector('.dark-mode-test').textContent = 'DarkMode';
+        document.querySelector('.navbar').style.boxShadow = '0px 1px 100px 0px rgba(0,0,0,0.1)';
     }
 });
