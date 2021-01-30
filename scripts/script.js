4bbcfd0fcb2d2d983bb1bf7fc4c5b0a675dac19c
Piotrek <piotrgszkola@gmail.com>
2021-01-30 15:48:57 +0100

    code

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,30 @@
-window.addEventListener('scroll', function() {
+function animate(div, text){
+  console.log('dziala funkjca')
+  var typed = new Typed(div, {
+    strings: [
+      text
+    ],
+    smartBackspace: false,
+    typeSpeed: 30
+  });
+}
 
+
+window.addEventListener('scroll', function(unchecked) {
     let scroll = window.pageYOffset;
-    
     if(scroll >= 200){
       document.querySelector('.container-div').classList.add('container-animation');
     }
-    
   });
 
-var typed = new Typed('.header-start-heading', {
-  strings: [
-    "Stwórzmy idealną strone dla Ciebie!"
-  ],
-  smartBackspace: false,
-  typeSpeed: 30
-});
+
+
+document.querySelector('body').onload = function(){
+  animate('.header-start-heading', 'Stwórzmy idealną strone dla Ciebie!')
+}
+
+
+
+
 
 
